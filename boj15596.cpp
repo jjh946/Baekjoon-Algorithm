0273e6bea54664415fe1fa555b331bb716f16afa
jjh946 <junhyeong.tech@gmail.com>
2021-11-25 14:11:53 +0900

    add boj10718
    강한친구 대한육군
    강한친구 대한육군

---
+++ boj15596.cpp
@@ -0,0 +1,11 @@
+//내가 집중해야 할 화면은 이것이다. 
+
+#include<iostream>
+using namespace std;
+
+long long sum(std::vector<int> &a);
+
+int main()
+{
+    
+}
--- hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "oh" << std::endl;
-    std::cout << "I dont know" << std::endl;
-    return 0;
-}

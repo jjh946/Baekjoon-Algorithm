ca00678fea573f42af43845fe7dd67c2128618e7
jjh946 <junhyeong.tech@gmail.com>
2021-12-01 19:25:38 +0900

    add boj2439
      *
     **
    ***

---
+++ 03_for/boj2439.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(NULL);
+    int n;
+
+    std::cin >> n;
+
+    
+
+
+    for(int i = 0; i<n; ++i)
+    {
+        for(int j = 0;j<n-i-1;++j)
+            std::cout << ' ';
+
+        for(int j = 0;j<=i;++j)
+            std::cout << '*';
+        std::cout << '\n';
+    }
+    return 0;
+}

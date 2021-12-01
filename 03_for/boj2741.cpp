2ce4fba4a07d6a0407b77cf7095e22c7c6f8cc45
jjh946 <junhyeong.tech@gmail.com>
2021-12-01 19:02:15 +0900

    add boj2741
    print 1~n

---
+++ 03_for/boj2741.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(NULL);
+    int n;
+
+    std::cin >> n;
+
+    
+
+
+    for(int i = 0; i<n; ++i)
+        std::cout << i+1 << '\n';
+
+   
+   
+    return 0;
+}

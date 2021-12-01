09908b9dc6c23e3fa11374c7c2a73043d2e73d2b
jjh946 <junhyeong.tech@gmail.com>
2021-12-01 19:12:57 +0900

    add boj11021
    A+B - 7

---
+++ 03_for/boj11021.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(NULL);
+    int n;
+
+    std::cin >> n;
+
+    int a[n], b[n];
+
+
+    for(int i = 0; i<n; ++i)
+        std::cin >> a[i] >> b[i];
+
+    for(int i = 0; i<n; ++i)
+        std::cout<< "Case #" << i+1 << ": " << a[i]+b[i] << '\n';
+   
+    return 0;
+}

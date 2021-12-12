a045d38bf87cefd54e28daee2563814b26088f77
jjh946 <junhyeong.tech@gmail.com>
2021-12-13 07:40:06 +0900

    boj15596
    function

---
+++ 04_while/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    
+    std::string oxstr;
+
+    std::cin >> oxstr;
+    
+        
+
+       
+    
+    std::cout << oxstr[0] << '\n' << oxstr[2] <<'\n';
+
+    return 0;
+}
